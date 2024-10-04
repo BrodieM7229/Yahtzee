737d3d13e6933a4af2fd8f017dc5c94bdc830c26
MiBlay07 <mi.blay@fwcsstudents.org>
2024-10-04 10:21:28 -0400

    dice_deck.py
    dice deck display next to each other

--- dice_deck_and_call_dice.py
+++ dice_deck_and_call_dice.py
@@ -1,55 +1,76 @@
-import random
-
 def create_dice_face(value):
     faces = {
-        1: """
-    +-------+
-    |       |
-    |   ●   |
-    |       |
-    +-------+
-    """,
-        2: """
-    +-------+
-    | ●     |
-    |       |
-    |     ● |
-    +-------+
-    """,
-        3: """
-    +-------+
-    | ●     |
-    |   ●   |
-    |     ● |
-    +-------+
-    """,
-        4: """
-    +-------+
-    | ●   ● |
-    |       |
-    | ●   ● |
-    +-------+
-    """,
-        5: """
-    +-------+
-    | ●   ● |
-    |   ●   |
-    | ●   ● |
-    +-------+
-    """,
-        6: """
-    +-------+
-    | ●   ● |
-    | ●   ● |
-    | ●   ● |
-    +-------+
-    """
+        1: [
+            "+-------+",
+            "|       |",
+            "|   ●   |",
+            "|       |",
+            "+-------+"
+        ],
+        2: [
+            "+-------+",
+            "| ●     |",
+            "|       |",
+            "|     ● |",
+            "+-------+"
+        ],
+        3: [
+            "+-------+",
+            "| ●     |",
+            "|   ●   |",
+            "|     ● |",
+            "+-------+"
+        ],
+        4: [
+            "+-------+",
+            "| ●   ● |",
+            "|       |",
+            "| ●   ● |",
+            "+-------+"
+        ],
+        5: [
+            "+-------+",
+            "| ●   ● |",
+            "|   ●   |",
+            "| ●   ● |",
+            "+-------+"
+        ],
+        6: [
+            "+-------+",
+            "| ●   ● |",
+            "| ●   ● |",
+            "| ●   ● |",
+            "+-------+"
+        ]
     }
-    return faces[value]
+    return faces[value]  # Return the face as a list of strings
 
-def roll_dice():
-    value = random.randint(1, 6)  # Generate a random number between 1 and 6
-    return create_dice_face(value)  # Call the function to get the dice face
 
-print("Rolling the dice.....")
-print(roll_dice())
+# Display all dice faces
+def display_all_dice_faces():
+    # Prepare to collect the lines for each dice face
+    top = []
+    middle1 = []
+    middle2 = []
+    middle3 = []
+    bottom = []
+
+    for value in range(1, 7):
+        face = create_dice_face(value)  # Get lines for the die face
+        top.append(face[0])  # Top line of the dice face
+        middle1.append(face[1])  # First middle line of the dice face
+        middle2.append(face[2])  # Second middle line of the dice face
+        middle3.append(face[3])  # Third middle line of the dice face
+        bottom.append(face[4])  # Bottom line of the dice face
+
+    # Print all lines together, ensuring alignment
+    print("  ".join(top))
+    print("  ".join(middle1))
+    print("  ".join(middle2))
+    print("  ".join(middle3))
+    print("  ".join(bottom))
+
+
+# Run the function to display all dice faces
+display_all_dice_faces()
+
