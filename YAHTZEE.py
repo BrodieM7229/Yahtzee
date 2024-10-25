e05a85fd75212b2f670b68984aff1b3451893b31
brodymchenry <brody.mchenry@fwcsstudents.org>
2024-10-25 08:30:13 -0400

    YAHTZEE.py

--- YAHTZEE.py
+++ YAHTZEE.py
@@ -1,4 +1,4 @@
-def yahtzee(roll:list):
+def yahtzee(roll:list) -> int:
     yahtzees = [
         [1,1,1,1,1],
         [2,2,2,2,2],
@@ -7,7 +7,7 @@ def yahtzee(roll:list):
         [5,5,5,5,5],
         [6,6,6,6,6]
     ]
-    if roll in yahtzees:
+    if sorted(roll) in yahtzees:
         print("YAHTZEE")
         return 50
     else:
